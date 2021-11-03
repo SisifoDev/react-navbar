467dda5da00f5d8e5a194e302488829b81edf598
sisifodev <manuelperdomo1710@icloud.com>
2021-11-03 10:59:26 -0500

    Router and Home page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import Home from "./pages";
 
 function App() {
   return (
     <Router>
       <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
     </Router>
   );
 }
